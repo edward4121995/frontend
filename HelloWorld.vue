5533d1b40f5b7e604202eb1274809efb72723b7f
edward4121995 <85068222+edward4121995@users.noreply.github.com>
2021-05-30 22:00:39 +0700

    Add files via upload

---
+++ HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>Todo List</h1>
+  <div>
+  <input v-model="myText">
+  <button @click="tambah">Add</button>
+  <ul>
+    <li v-for='item in todos'>{{ item.desc }}</li>
+  </ul>
+  </div>
+</template>
+
+<script >
+
+export default{
+  data: function(){
+    return {
+      todos: [
+        {desc: 'Bangun Tidur'},
+        {desc: 'Mandi'},
+        {desc: 'Makan'}
+      ],
+      myText : ''
+    }
+  },
+  methods: {
+    tambah: function(){
+      this.todos.push({ desc: this.myText})
+    }
+  }
+}
+
+</script>
+
+
